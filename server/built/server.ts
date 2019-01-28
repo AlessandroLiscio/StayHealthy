3b95a4cdcbcbf55f731566d007f4967faa1f31e6
Rocche <giacomorocchetti97@gmail.com>
2019-01-28 09:17:39 +0100

    Caching messaggi

--- server/built/server.ts
+++ server/built/server.ts
@@ -54,7 +54,7 @@ app.use(passport.session()); // persistent login sessions
 require('./routes.ts')(app, passport); // load our routes and pass in our app and fully configured passport
 
 // launch ======================================================================
- https.createServer(certOptions, app).listen(443, () => console.log('Server listening on port: 3000'));
+ https.createServer(certOptions, app).listen(443, () => console.log('Server listening on port: 443'));
  app.listen(80, ()=> console.log("Listening on port 80"));
 //app.listen(process.env.PORT);
 //console.log('Server listening on port: ' + process.env.PORT);
